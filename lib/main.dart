bd31b035819e2b635938b7fab5d42c362e888a28
Fady2024 <fadygerges2023@gmail.com>
2025-07-31 01:23:19 +0300

    🚀 Initialize Flutter application

---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'core/theme/app_theme.dart';
+import 'package:flutter/foundation.dart' show kIsWeb;
+import 'features/layout/presentation/widgets/phone_frame_wrapper.dart';
+import 'features/welcome/presentation/screens/welcome_screen.dart';
+import 'l10n/app_localizations.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'GreeAlex',
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme.lightTheme,
+      localizationsDelegates: const [
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [
+        Locale('en'), // English
+        Locale('ar'), // Arabic
+      ],
+            builder: (context, child) {
+        if (kIsWeb) {
+          return PhoneFrameWrapper(child: child!);
+        }
+        return child!;
+      },
+      home: const WelcomeScreen(),
+    );
+  }
+}
