557d0c1c740851b77081f9d5b8abd8b9aaba2ef9
Fady2024 <fadygerges2023@gmail.com>
2025-07-31 00:58:32 +0300

    🔧 Add core constants and asset paths

---
+++ lib/core/constants/assets.dart
@@ -0,0 +1,12 @@
+class AppAssets {
+  // Logos
+  static const String alexBankLogo = 'assets/images/alexbank_logo.png';
+  static const String appLogo = 'assets/images/gree_alex_logo.png';
+  
+  // Icons
+  static const String leafIcon = 'assets/icons/leaf.svg';
+  static const String treeIcon = 'assets/icons/tree.svg';
+  
+  // Illustrations
+  static const String welcomeBackground = 'assets/images/welcome_bg.svg';
+} 
